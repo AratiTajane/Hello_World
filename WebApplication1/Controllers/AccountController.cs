ab009ec4baf639d393b68638a5e4ed5dfdd40260
arati.shinde <arati.shinde@RPSPLDP1-041>
2017-11-03 15:35:07 +0530

    Added string.

--- WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/Controllers/AccountController.cs
@@ -20,6 +20,7 @@ namespace WebApplication1.Controllers
 
         public AccountController()
         {
+            string abc = "Arati";
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
